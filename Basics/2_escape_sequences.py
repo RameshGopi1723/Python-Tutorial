97d9aae6cb476a671366e76985f176252d9dd837
RameshGopi1723 <rameshgopi1723@gmail.com>
2025-04-05 14:24:14 +0530

    Added some basics contents of python

---
+++ Basics/2_escape_sequences.py
@@ -0,0 +1,9 @@
+print("Escape\nSequence\n")
+
+print("Escape\tSequence\t")
+
+print('This is Siva\'s Phone')
+
+print("Escape\\Sequence\\")
+
+print('\'It\'s Hero Time!\'\n\t\t- Ben 10')
---
+++ Basics/5_data_types.py
@@ -0,0 +1,10 @@
+#Python data types
+#Python has the following data types built-in by default, in these categories:
+#simple data types and complex data types
+#Simple data types
+#Text Type:	str
+#Numeric Types:	int, float
+#Boolean Type:	bool
+#Sequence Types:	list, tuple, dict
+#Set Types:	set
+
