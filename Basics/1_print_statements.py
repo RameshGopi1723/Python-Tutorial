e94566e2a2a05303962fd306f8bfd96515c9979f
RameshGopi1723 <rameshgopi1723@gmail.com>
2025-04-05 14:29:48 +0530

    we will start from the scratch

--- Basics/1_print_statements.py
+++
@@ -1,7 +0,0 @@
-print("Om Nama Shivaya..!","Shivaya Namaha", end="..!")
-
-print("Hara Hara Mahadev..!","Mahadev Hara", end="..!", sep="_")
-
-print("Venkateshwara Swamy Govinda..!","Govinda Govinda", end="..!", sep="_", file=open("output.txt", "w"))
-
-print("Jai Shri Ram..!","Jai Shri Ram", end="..!", sep="_", flush=True) #flush is used to print the output immediately without waiting for the buffer to fill
--- Basics/2_escape_sequences.py
+++
@@ -1,9 +0,0 @@
-print("Escape\nSequence\n")
-
-print("Escape\tSequence\t")
-
-print('This is Siva\'s Phone')
-
-print("Escape\\Sequence\\")
-
-print('\'It\'s Hero Time!\'\n\t\t- Ben 10')
